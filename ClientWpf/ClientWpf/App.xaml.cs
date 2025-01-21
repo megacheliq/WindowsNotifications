2a04a87d23f40a0760a849fae3a2be608ec2b3fe
megacheliq <121279842+megacheliq@users.noreply.github.com>
2025-01-21 14:15:35 +0500

    Update App.xaml.cs

--- ClientWpf/ClientWpf/App.xaml.cs
+++ ClientWpf/ClientWpf/App.xaml.cs
@@ -5,16 +5,16 @@ namespace ClientWpf
     public partial class App : Application
     {
         private TcpNotificationClient? _tcpNotificationClient;
-
+ 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-
-            _tcpNotificationClient = new TcpNotificationClient("192.168.0.103", 5000);
+ 
+            _tcpNotificationClient = new TcpNotificationClient("192.168.88.39", 57014);
             Task.Run(() => _tcpNotificationClient.StartAsync());
             ShutdownMode = ShutdownMode.OnExplicitShutdown;
         }
-
+ 
         protected override void OnExit(ExitEventArgs e)
         {
             _tcpNotificationClient?.Stop();
