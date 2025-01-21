c9cbee5cb4bdeb4e28a18bdaadf8fbf77cc44872
megacheliq <121279842+megacheliq@users.noreply.github.com>
2025-01-21 14:16:47 +0500

    Update NotificationWindow.xaml.cs

--- ClientWpf/ClientWpf/NotificationWindow.xaml.cs
+++ ClientWpf/ClientWpf/NotificationWindow.xaml.cs
@@ -1,10 +1,11 @@
-﻿using System.Media;
+using System.Media;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Threading;
-
+ 
 namespace ClientWpf;
-
+ 
 public partial class NotificationWindow : Window
 {
     private readonly TcpNotificationClient _client;
@@ -12,22 +13,23 @@ public partial class NotificationWindow : Window
     private DispatcherTimer _flashTimer;
     private bool _isSoundPlaying = true;
     private SolidColorBrush _red = new SolidColorBrush(Colors.Red);
-    private SolidColorBrush _yellow = new SolidColorBrush(Colors.Yellow);
-
+    private SolidColorBrush _white = new SolidColorBrush(Colors.White);
+    private SolidColorBrush _black = new SolidColorBrush(Colors.Black);
+ 
     public NotificationWindow(string title, string message, TcpNotificationClient client)
     {
         InitializeComponent();
-        TitleBlock.Text = title;
-        MessageBlock.Text = message;
+        //TitleBlock.Text = title;
+        //MessageBlock.Text = message;
         _client = client;
         _message = message;
-        
+ 
         SetupFlashing();
         PlayAnnoyingSoundInLoop();
-        
+ 
         Loaded += OnLoaded;
     }
-    
+ 
     private void SetupFlashing()
     {
         _flashTimer = new DispatcherTimer
@@ -37,28 +39,38 @@ public partial class NotificationWindow : Window
         _flashTimer.Tick += FlashWindow;
         _flashTimer.Start();
     }
-    
+ 
     private void FlashWindow(object? sender, EventArgs e)
     {
-        GridBackground.Background = GridBackground.Background == _red ? _yellow : _red;
+        GridBackground.Background = GridBackground.Background == _red ? _white : _red;
+ 
+        var stackPanel = (StackPanel)GridBackground.Children[0];
+ 
+        foreach (var child in stackPanel.Children)
+        {
+            if (child is TextBlock textBlock)
+            {
+                textBlock.Foreground = textBlock.Foreground == _white ? _black : _white;
+            }
+        }
     }
-
+ 
     private async void PlayAnnoyingSoundInLoop()
     {
         while (_isSoundPlaying)
         {
             SystemSounds.Beep.Play();
-
+ 
             await Task.Delay(1000);
         }
     }
-
+ 
     private void OnLoaded(object sender, RoutedEventArgs e)
     {
         Left = SystemParameters.WorkArea.Left + 10;
         Top = SystemParameters.WorkArea.Bottom - Height - 10;
     }
-
+ 
     private void CloseNotification(object sender, RoutedEventArgs e)
     {
         _client.SendReadConfirmation(_message);
